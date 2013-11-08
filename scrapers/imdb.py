4f6382a51d455f788ed1c0fbd6e7343b78259b7a
Hayden Eskriett <hayden.eskriett@gmail.com>
2013-11-08 22:28:27 +0000

    Optimised the code.
    Now making use of the streamlined IMDB mobile page to grab the shows rating. Alsoptimised the Beautiful Soup code.

--- scrapers/imdb.py
+++ scrapers/imdb.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
-
 from bs4 import BeautifulSoup
 import urllib2
 import sys
 
-show = sys.argv[1]
-html = urllib2.urlopen("http://www.imdb.com/title/{0}".format(show))
-source = html.read()
-soup = BeautifulSoup(source)
-
-rating_elements = soup.find_all('div', 'titlePageSprite star-box-giga-star')
-print rating_elements[0].contents[0]
+showid = sys.argv[1]
+html = urllib2.urlopen("http://m.imdb.com/title/{0}".format(showid))
+soup = BeautifulSoup(html.read())
+rating_elements = soup.find("p", {"class":"votes"})
+print rating_elements.strong.string
