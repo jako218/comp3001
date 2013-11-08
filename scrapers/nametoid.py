6c7388a61f7b6812bf36346535948c0e898d5b59
Jamie Davies <jamie@jamiedavies.me>
2013-11-08 13:34:28 +0000

    Added a script to get the TVDB ID for a show from it's name.

---
+++ scrapers/nametoid.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from bs4 import BeautifulSoup
+import urllib2
+import sys
+
+show_name = urllib2.quote('_'.join(sys.argv[1:]))
+
+tvdbxml = urllib2.urlopen("http://thetvdb.com/api/GetSeries.php?seriesname={0}".format(show_name))
+tvdbsoup = BeautifulSoup(tvdbxml.read(), 'xml')
+
+for hit in tvdbsoup.find_all('Series'):
+    print hit.find('id').string
+
