cc4fc70bbfffc25af99c54f714c1e72dc8a95bba
Hayden Eskriett <hayden.eskriett@gmail.com>
2014-01-07 17:32:27 +0000

    Added comment to manage

--- telehex/manage.py
+++ telehex/manage.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+"""
+:mod:`manage`
+=================================================
+
+.. module:: manage
+   :synopsis: Required Django setup file
+
+.. moduleauthor:: Miles Armstrong <mhha1g11@ecs.soton.ac.uk>
+.. moduleauthor:: Simon Bidwell <sab3g11@ecs.soton.ac.uk>
+.. moduleauthor:: Will Buss <wjb1g11@ecs.soton.ac.uk>
+.. moduleauthor:: Jamie Davies <jagd1g11@ecs.soton.ac.uk>
+.. moduleauthor:: Hayden Eskriett <hpe1g11@ecs.soton.ac.uk>
+.. moduleauthor:: Jack Flann <jof1g11@ecs.soton.ac.uk>
+.. moduleauthor:: Chantel Spencer-Bowdage <csb1g11@ecs.soton.ac.uk>
+"""
 import os
 import sys
 
