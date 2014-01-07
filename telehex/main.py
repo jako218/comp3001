2400ff8a3d25e2741ea5d09101bfe28e8ae94b54
Hayden Eskriett <hayden.eskriett@gmail.com>
2014-01-07 17:37:00 +0000

    Added comment to main.py

--- telehex/main.py
+++ telehex/main.py
@@ -1,3 +1,19 @@
+"""
+:mod:`main`
+=================================================
+
+.. module:: main
+   :synopsis: Main python file for django. Adds the libs folder to the path
+
+.. moduleauthor:: Miles Armstrong <mhha1g11@ecs.soton.ac.uk>
+.. moduleauthor:: Simon Bidwell <sab3g11@ecs.soton.ac.uk>
+.. moduleauthor:: Will Buss <wjb1g11@ecs.soton.ac.uk>
+.. moduleauthor:: Jamie Davies <jagd1g11@ecs.soton.ac.uk>
+.. moduleauthor:: Hayden Eskriett <hpe1g11@ecs.soton.ac.uk>
+.. moduleauthor:: Jack Flann <jof1g11@ecs.soton.ac.uk>
+.. moduleauthor:: Chantel Spencer-Bowdage <csb1g11@ecs.soton.ac.uk>
+"""
+
 import django.core.handlers.wsgi
 import sys
 
